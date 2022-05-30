d552d4903b6e050e4aa31b52929980c6c2074a14
MaximeMgtx <maximemagotteaux02@hotmail.com>
2022-05-30 13:52:30 +0200

    card créée

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import {Stack} from '@mui/material';
+import {Button, Card, CardActions, CardContent, Fab, Grid, Stack, Typography} from '@mui/material';
 import { Container } from '@mui/system';
 import MenuBar from './Composants/MenuBar/MenuBar';
+import AddIcon from '@mui/icons-material/Add';
 
 
 function App() {
@@ -8,7 +9,25 @@ function App() {
     <Stack>
       <MenuBar />
       <Container maxWidth="md">
-        <p>Hello</p>
+        <Stack spacing={2}>
+          <h2>Titre</h2>
+          <Grid container spacing={2}>
+            <Grid item sm={6} md={4}>
+            <Card>
+              <CardContent>
+                <Typography variant="h5" component="div">
+                  Elément
+                </Typography>
+              </CardContent>
+          </Card>
+            </Grid>
+          </Grid>
+          <Fab color='primary' aria-label='add'>
+            <AddIcon/>
+
+          </Fab>
+        </Stack>
+        
       </Container>
       
     </Stack>
